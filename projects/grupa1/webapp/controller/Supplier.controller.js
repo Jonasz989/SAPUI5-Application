dbfb9961204f3203e74d6aeb63e2223f7d6c5aee
Damian Jabłoński <dj.damianjablonski@gmail.com>
2022-08-30 08:30:08 +0000

    essa

---
+++ projects/grupa1/webapp/controller/Supplier.controller.js
@@ -0,0 +1,33 @@
+sap.ui.define([
+    "./BaseController",
+    "sap/ui/model/json/JSONModel",
+], function(BaseController, JSONModel) {
+    'use strict';
+    
+    return BaseController.extend("masterdetail.controller.Supplier", {
+
+        onInit: function () {
+            var oViewModel = new JSONModel({
+                busy : false,
+                delay : 0
+            });
+
+            this.getRouter().getRoute("supp").attachPatternMatched(this._onObjectMatched, this);
+
+            this.setModel(oViewModel, "supplierView");
+        },
+
+        _onObjectMatched: function (oEvent) {
+            var sObjectId = oEvent.getParameter("arguments").ObjectId;
+            this.getModel("appView").setProperty("/layout", "OneColumn");
+            this.getModel().metadataLoaded().then( function() {
+                this._bindView("/Products("+ sObjectId + ")/Supplier");
+            }.bind(this));
+        },
+        _bindView: function (sObjectPatch) {
+            this.getView().bindElement({
+                path : sObjectPatch
+            });
+        }
+    })
+});
