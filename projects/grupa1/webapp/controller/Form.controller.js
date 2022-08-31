56ddd91299e79db254d31103c8ca2aa3cdc19f76
jwysm <joanna.wysmulek@outlook.com>
2022-08-31 11:37:33 +0000

    form goback

--- projects/grupa1/webapp/controller/Form.controller.js
+++ projects/grupa1/webapp/controller/Form.controller.js
@@ -13,8 +13,26 @@ sap.ui.define([
             this.getModel("appView").setProperty("/layout", "OneColumn");
         },
 
-        onSaveProductClick: function (oEvent) {
+        onSaveProductClick: function () {
+            var oCat={
+                "ID": Math.floor(Math.random()*151)-5,
+                "Name": document.getElementById("prname").value,
+                "Description": document.getElementById("prdesc").value,
+                "Released Date": document.getElementById("prreldate").value,
+                "Discontinued Date": document.getElementById("prdiscdate").value,
+                "Rating": document.getElementById("prrating").value,
+                "Price": document.getElementById("prprice").value,
+                "Category": document.getElementById("prcat").value,
+                "Supplier": document.getElementById("prsup").value
+            }
+            var oModel = this.getView().byId("detailView").getModel();
 
+            oModel.create("/Categories/1", oCat, {
+                success: function () { MessageToast.show("Success!"); },
+                error: function (oError) { MessageToast.show("Something went wrong :c"); }
+            });
+
+            history.back();
         }
     });
 });
