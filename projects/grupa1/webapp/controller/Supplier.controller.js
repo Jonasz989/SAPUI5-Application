42cbc3b83ccd16b1ad6e11ad722a52643eb6d8d2
jwysm <joanna.wysmulek@outlook.com>
2022-09-01 08:03:39 +0000

    wlekfn

--- projects/grupa1/webapp/controller/Supplier.controller.js
+++ projects/grupa1/webapp/controller/Supplier.controller.js
@@ -1,7 +1,25 @@
 sap.ui.define([
     "./BaseController",
-    "sap/ui/model/json/JSONModel"
-], function(BaseController, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/Sorter",
+    "sap/ui/model/FilterOperator",
+    "sap/m/GroupHeaderListItem",
+    "sap/ui/Device",
+    "sap/ui/core/Fragment",
+    "../model/formatter",
+    "sap/m/Dialog",
+    "sap/m/DialogType",
+    "sap/m/Button",
+    "sap/m/ButtonType",
+    "sap/m/Text",
+    "sap/m/MessageToast",
+    "sap/m/MessageBox",
+    "sap/m/Input"
+
+], function (BaseController, JSONModel, Filter, Sorter, FilterOperator, GroupHeaderListItem, Device, Fragment, formatter, Dialog, DialogType, Button, ButtonType, Text,
+    MessageToast, MessageBox, Input) {
+
     "use strict";
     
     return BaseController.extend("grupa1.controller.Supplier", {
@@ -31,6 +49,81 @@ sap.ui.define([
                     expand : "Supplier"
                 }
             });
+        },
+
+        onProductUpdateClick: function (oItem) {
+            //const cos = oItem.getBindingContext();
+            this.oApproveDialog = new Dialog({
+                type: DialogType.Message,
+                title: "Edit Product",
+                content:[
+
+                    new sap.m.Label({text:"Name"}),
+                    new sap.m.Input({
+
+                        maxLength: 20,
+                        id: "PName"
+
+                    }),
+
+                    new sap.m.Label({text:"Description"}),
+                    new sap.m.Input({
+
+                        maxLength: 20,
+                        id: "PDesc"
+
+                    }),
+
+                    new sap.m.Label({text:"Rating"}),
+                    new sap.m.Input({
+
+                        maxLength: 1,
+                        id: "PRating" 
+
+                    }),
+
+                    new sap.m.Label({text:"Price"}),
+                    new sap.m.Input({
+
+                        maxLength: 7,
+                        id: "PPrice" 
+                  })
+                ],
+                beginButton: new Button({
+                    type: ButtonType.Emphasized,
+                    text: "Submit",
+                    press: function () {
+
+                        var prodname = sap.ui.getCore().byId("PName").getValue();
+                        var proddesc = sap.ui.getCore().byId("PDesc").getValue();
+                        var prodrating = sap.ui.getCore().byId("PRating").getValue();
+                        var prodprice = sap.ui.getCore().byId("PPrice").getValue();
+
+                        var oCat ={
+                            if(prodname.length != 0){
+                                "Name": prodname
+                            }
+
+                            }
+                        var oModel = this.getView().getModel();
+
+                        oModel.create("/Categories", oCat, {
+                            success: function () { MessageToast.show("Success!"); },
+                            error: function (oError) { MessageToast.show("Something went wrong :c"); }
+                        });
+                        this.oApproveDialog.destroy();
+                    }.bind(this)
+                }),
+                endButton: new Button({
+                    text: "Cancel",
+                    press: function() {
+                        this.oApproveDialog.destroy();
+                    }.bind(this)
+                })
+            });
+
+            this.oApproveDialog.open();
+
         }
     })
 });
