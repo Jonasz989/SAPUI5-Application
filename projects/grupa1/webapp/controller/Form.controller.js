c5e9cfbbce5b6a425593c7dd5aaf638f971ac032
jwysm <joanna.wysmulek@outlook.com>
2022-09-01 12:07:23 +0000

    walidacja

--- projects/grupa1/webapp/controller/Form.controller.js
+++ projects/grupa1/webapp/controller/Form.controller.js
@@ -1,6 +1,24 @@
 sap.ui.define([
-    "./BaseController"
-], function (BaseController) {
+    "./BaseController",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/Sorter",
+    "sap/ui/model/FilterOperator",
+    "sap/m/GroupHeaderListItem",
+    "sap/ui/Device",
+    "sap/ui/core/Fragment",
+    "../model/formatter",
+    "sap/m/Dialog",
+    "sap/m/DialogType",
+    "sap/m/Button",
+    "sap/m/ButtonType",
+    "sap/m/Text",
+    "sap/m/MessageToast",
+    "sap/m/MessageBox",
+    "sap/m/Input"
+
+], function (BaseController, JSONModel, Filter, Sorter, FilterOperator, GroupHeaderListItem, Device, Fragment, formatter, Dialog, DialogType, Button, ButtonType, Text,
+    MessageToast, MessageBox, Input) {
     "use strict";
 
     return BaseController.extend("grupa1.controller.Form", {
@@ -12,6 +30,7 @@ sap.ui.define([
         _onFormDisplayed : function () {
             this.getModel("appView").setProperty("/layout", "OneColumn");
         },
+        
 
         onSaveProductClick: function () {
             console.log(this.getView().byId("prprice").getValue());
